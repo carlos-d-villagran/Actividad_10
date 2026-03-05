be2f5aa88ac420b60b04ed498f9f08a7a27ec4ec
carlos-d-villagran <carlos.danivillagran27@gmail.com>
2026-03-04 22:48:16 -0600

    Corrección de ortografía

--- Ejercicio4/Ejercicio4/Program.cs
+++ Ejercicio4/Ejercicio4/Program.cs
@@ -16,9 +16,10 @@ namespace Ejercicio4
             ciudad.Add(9003, "Olintepeque");
             ciudad.Add(9004, "San Carlos Sija");
             ciudad.Add(9032, "La Esperanza");
-            Console.WriteLine("Indicque el codigo postal y se desplegará la ciudad: ");
+            Console.WriteLine("Indique el codigo postal y se desplegará la ciudad: ");
             int codigo = int.Parse(Console.ReadLine());
             Console.WriteLine($"El código: {codigo} pertenece a:  {ciudad[codigo]}");
+            //El diccionario no toma en cuenta el cero a la izquierda
         }
     }
 }
