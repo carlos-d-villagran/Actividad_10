fef861393f1738ebeb60a03bcce2b660e79ecbf9
carlos-d-villagran <carlos.danivillagran27@gmail.com>
2026-03-04 22:42:56 -0600

    Ejercicio3

---
+++ Ejercicio4/Ejercicio4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> ciudad = new Dictionary<int, string>();
+            ciudad.Add(9001, "Quetzaltenango");
+            ciudad.Add(9002, "Salcajá");
+            ciudad.Add(9003, "Olintepeque");
+            ciudad.Add(9004, "San Carlos Sija");
+            ciudad.Add(9032, "La Esperanza");
+            Console.WriteLine("Indicque el codigo postal y se desplegará la ciudad: ");
+            int codigo = int.Parse(Console.ReadLine());
+            Console.WriteLine($"El código: {codigo} pertenece a:  {ciudad[codigo]}");
+        }
+    }
+}
