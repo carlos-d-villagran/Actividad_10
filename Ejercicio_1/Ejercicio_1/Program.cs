e31dd0742db2c22a4697a599465e5accb7808104
carlos-d-villagran <carlos.danivillagran27@gmail.com>
2026-03-04 22:21:25 -0600

    Ejercicio1
    Diccionario de estudiantes

---
+++ Ejercicio_1/Ejercicio_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> estudiantes = new Dictionary<int, string>();
+            estudiantes.Add(1010, "Juan Pérez");
+            estudiantes.Add(1011, "Carlos López");
+            estudiantes.Add(1012, "Antonio Flores");
+            Console.WriteLine("Indique el carné del estudiante a buscar: ");
+            int carne = int.Parse(Console.ReadLine());
+            Console.WriteLine(estudiantes[carne]);
+        }
+    }
+}
