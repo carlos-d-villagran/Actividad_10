686e39169f67f5bd9009d4d5ca4cacc4634010a1
carlos-d-villagran <carlos.danivillagran27@gmail.com>
2026-03-04 22:26:48 -0600

    Ejercicio2

---
+++ EJERCICIO2/EJERCICIO2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJERCICIO2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> productos = new Dictionary<int, string>();
+            productos.Add(1010, "Mayonesa B&B");
+            productos.Add(1011, "Shampoo Lagrimitas");
+            productos.Add(1012, "Galletas Maria");
+            foreach (var item in productos)
+            {
+                Console.WriteLine($"Código: {item.Key}  Nombre: {item.Value}");
+            }
+        }
+    }
+}
