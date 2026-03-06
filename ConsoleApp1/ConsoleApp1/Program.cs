8322ed89a0580c86b3d2f6872b2153b3ad379fc6
carlos-d-villagran <carlos.danivillagran27@gmail.com>
2026-03-05 18:54:39 -0600

    Ejercicio7

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> productos = new Dictionary<int, string>();
+            productos.Add(1011, "Polvo mágico");
+            productos.Add(1012, "Fabuloso");
+            productos.Add(1013, "Galletas María");
+            int id;
+            string nombre;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Ingrese un nuevo producto:");
+                Console.Write("Ingrese el nombre del producto: ");
+                nombre = Console.ReadLine();
+                Console.Write("Ingrese su clave: ");
+                id = int.Parse(Console.ReadLine());
+                if(productos.ContainsKey(id))
+                {
+                    Console.WriteLine("Error, este producto ya existe");
+                }
+            }
+            while (productos.ContainsKey(id));
+            productos.Add(id, nombre);
+            foreach(var item in productos)
+            {
+                Console.WriteLine($"Código: {item.Key}  Producto: {item.Value}");
+            }
+        }
+    }
+}
