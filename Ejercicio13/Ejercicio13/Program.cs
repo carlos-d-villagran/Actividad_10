d1cd972a8e42dad1085dacdaa5268c0c8dcd8eb6
carlos-d-villagran <carlos.danivillagran27@gmail.com>
2026-03-05 23:29:31 -0600

    Ejercicio13

---
+++ Ejercicio13/Ejercicio13/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, double> estudiantes = new Dictionary<string, double>();
+            estudiantes.Add("Hernesto Valverde", 45.5);
+            estudiantes.Add("Miguel Martinez", 100 );
+            estudiantes.Add("Julio Hernandez", 78.4);
+            estudiantes.Add("Pedro Sanchez", 45.1);
+            estudiantes.Add("Farid Jimenez", 67.1);
+            double suma = 0;
+            
+            foreach(var notas in estudiantes)
+            {
+                Console.WriteLine($"Nombre: {notas.Key}  Nota: {notas.Value}");
+                suma += notas.Value;
+            }
+            Console.WriteLine("\nEl promedio de las notas es: "+ suma/estudiantes.Count);
+
+        }
+    }
+}
