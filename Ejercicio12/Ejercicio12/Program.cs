26d3db92554480724ae6199bbb7ed14026a19ecd
carlos-d-villagran <carlos.danivillagran27@gmail.com>
2026-03-05 23:13:06 -0600

    Ejercicio12

---
+++ Ejercicio12/Ejercicio12/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> productos = new Dictionary<int, string>();
+            productos.Add(1010, "Mayonesa");
+            productos.Add(1011, "Galletas");
+            productos.Add(1012, "Shampoo");
+            productos.Add(1013, "Crema");
+            productos.Add(1014, "Jamón");
+            productos.Add(1015, "Papel de regalo");
+            productos.Add(1016, "Chocolotes");
+            foreach(var item in productos)
+            {
+                Console.WriteLine($"Código: {item.Key}  Producto: {item.Value}");
+            }
+        }
+    }
+}
