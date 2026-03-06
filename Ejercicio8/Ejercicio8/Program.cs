9859a74048dcebf2fb7ad1d49ba92869aaed2855
carlos-d-villagran <carlos.danivillagran27@gmail.com>
2026-03-05 19:02:35 -0600

    Ejercicio8

---
+++ Ejercicio8/Ejercicio8/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> empleados = new Dictionary<int, string>();
+            empleados.Add(1011, "Renato Pelicó");
+            empleados.Add(1012, "Miguel Martinez");
+            empleados.Add(1013, "Rosa Celeste");
+            empleados.Add(1014, "Maria Josefina");
+            Console.WriteLine("\nListado de empleados:");
+            foreach(var item in empleados)
+            {
+                Console.WriteLine($"Código: {item.Key}  Nombre: {item.Value}");
+            }
+            Console.WriteLine("\nIntroduzca el código de empleado a eliminar:");
+            int codigo = int.Parse( Console.ReadLine() );
+            empleados.Remove( codigo );
+            foreach (var item in empleados)
+            {
+                Console.WriteLine($"Código: {item.Key}  Nombre: {item.Value}");
+            }
+        }
+    }
+}
