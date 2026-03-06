f4e026000cf2333626b27d637683e63e85f37a46
carlos-d-villagran <carlos.danivillagran27@gmail.com>
2026-03-05 23:04:18 -0600

    Ejercicio10

---
+++ ejercicio10/ejercicio10/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> inventario = new Dictionary<int, string>();
+            inventario.Add(1010, "Crema");
+            inventario.Add(1011, "Shmapoo");
+            inventario.Add(1012, "Galleta");
+            inventario.Add(1013, "Cocacola");
+            inventario.Add(1014, "Servilletas");
+            Console.WriteLine("PRODUCTOS DISPONIBLES: ");
+            foreach (var item in inventario)
+            {
+                Console.WriteLine($"Código: {item.Key}  Producto: {item.Value}");
+            }
+            Console.Write("\nIngrese el código del producto a eliminar: ");
+            int codigo = int.Parse(Console.ReadLine());
+            inventario.Remove(codigo);
+            Console.WriteLine("\nProdcuto eliminado correctamente\n Listado actualizado:\n");
+            foreach (var item in inventario)
+            {
+                Console.WriteLine($"Código: {item.Key}  Producto: {item.Value}");
+            }
+
+        }
+    }
+}
