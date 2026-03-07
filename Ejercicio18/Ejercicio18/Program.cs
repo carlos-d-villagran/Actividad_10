32a1f438f0ecaf9880bf68b93cce031106529a35
carlos-d-villagran <carlos.danivillagran27@gmail.com>
2026-03-06 23:47:21 -0600

    Ejercicio18

---
+++ Ejercicio18/Ejercicio18/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> usuario = new Dictionary<int, string>();
+            int codigo;
+            do
+            {
+                Console.Write("Ingrese el código del nuevo usuario o 0 para finalizar: ");
+                do
+                {
+                    codigo= int.Parse(Console.ReadLine());
+                }
+                while (usuario.ContainsKey(codigo));
+                if(codigo!=0)
+                {
+                    Console.Write("Ingrese el nombre del usuario: ");
+                    string nombre = Console.ReadLine();
+                    usuario.Add(codigo, nombre);
+                }
+                else
+                {
+                    break;
+                }
+
+            }
+            while (true);
+            Console.WriteLine("\nListado de usuarios:\n");
+            foreach(var item in usuario)
+            {
+                Console.WriteLine($"ID: {item.Key}  Nombre: {item.Value}");
+            }
+        }
+    }
+}
